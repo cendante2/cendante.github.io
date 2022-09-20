fc143b5b5f155395270e16e43b81a1fe04269a43
cendante <antonio.cendanteijeiro@amey.co.uk>
2022-09-20 17:15:52 +0100

    My first commit

---
+++ app-files/data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama-of-modern-white-empty-loft-apartment-interior-living-hall-room-full-360-seamless-panorama-in-equirectangular-spherical-projection-skybox-v-rdfbca",
+      "name": "panorama-of-modern-white-empty-loft-apartment-interior-living-hall-room-full-360-seamless-panorama-in-equirectangular-spherical-projection-skybox-v-RDFBCA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
